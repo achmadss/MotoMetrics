cb1abdf6a4b98a4a5bad4e3425b7a80529b56647
achmadss <anakinskywalk1@gmail.com>
2023-11-25 10:57:12 +0700

    feat: DataState to handle reactivity

---
+++ data/src/main/java/com/achmadss/data/DataState.kt
@@ -0,0 +1,7 @@
+package com.achmadss.data
+
+sealed class DataState<out T> {
+    data object Loading : DataState<Nothing>()
+    data class Success<out Result>(val data: Result) : DataState<Result>()
+    data class Error(val error: Throwable) : DataState<Nothing>()
+}
