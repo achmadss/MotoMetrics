3a579dc8bc7f3ce97bac9a641e68e0dc7a16464f
achmadss <anakinskywalk1@gmail.com>
2023-11-26 09:44:52 +0700

    fix: Add Vehicle

--- app/src/main/java/com/achmadss/motometrics/ui/screens/add_vehicle/AddVehicleUIState.kt
+++
@@ -1,8 +0,0 @@
-package com.achmadss.motometrics.ui.screens.add_vehicle
-
-import com.achmadss.motometrics.base.BaseUIState
-
-class AddVehicleUIState(
-    override val loading: Boolean = false,
-    override val errorMessage: String = "Unknown error"
-) : BaseUIState
