ae6519b4ad6f8ca7642d45d29f4efca9f669962e
achmadss <anakinskywalk1@gmail.com>
2023-11-26 09:50:29 +0700

    refactor: remove unused Routes

--- app/src/main/java/com/achmadss/motometrics/Routes.kt
+++ app/src/main/java/com/achmadss/motometrics/Routes.kt
@@ -3,9 +3,6 @@ package com.achmadss.motometrics
 object Routes {
 
     const val MAIN = "main"
-    const val VEHICLE = "vehicle"
-    const val TRANSACTION = "transaction"
-
     const val ADD_VEHICLE = "add-vehicle"
     const val ADD_TRANSACTION = "add-transaction"
 
